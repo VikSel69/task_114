8d06f4749ad93597eb16ff2465e35a03de529184
vesel <v.e.seleznev@mail.ru>
2020-09-09 20:27:28 +0800

    Task_113

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,7 +5,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,30 +14,33 @@ import java.util.Properties;
 public class Util {
     private static SessionFactory sessionFactory;
 
-    public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration();
-                Properties settings = new Properties();
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/testbd?useSSL=false&useUnicode=true&serverTimezone=UTC");
-                settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-                settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "root");
-                settings.put(Environment.SHOW_SQL, "true");
-                settings.put(Environment.HBM2DDL_AUTO, "update");
-                configuration.setProperties(settings);
+    static {
+        try {
+            Properties prop = new Properties();
+            prop.put(Environment.URL, "jdbc:mysql://localhost:3306/testbd" +
+                    "?useSSL=false&useUnicode=true&serverTimezone=UTC");
+            prop.put(Environment.USER, "root");
+            prop.put(Environment.PASS, "root");
+            prop.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+            prop.put(Environment.SHOW_SQL, true);
+            prop.put(Environment.HBM2DDL_AUTO, "update");
+            prop.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
 
-                configuration.addAnnotatedClass(User.class);
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties()).build();
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            Configuration configuration = new Configuration().setProperties(prop);
+            System.out.println(configuration.getProperties());
+            configuration.addAnnotatedClass(User.class);
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties());
+            sessionFactory = configuration.buildSessionFactory(builder.build());
+        } catch (Exception e) {
+            System.out.println("Исключение!" + e);
         }
+    }
+
+    public static SessionFactory getSessionFactory() {
         return sessionFactory;
     }
+
     public static Connection getMySQLConnection() throws SQLException {
         String hostName = "localhost";
         String dbName = "testbd";
@@ -46,8 +48,10 @@ public class Util {
         String password = "root";
         return getMySQLConnection(hostName, dbName, userName, password);
     }
-    private static Connection getMySQLConnection(String hostName, String dbName, String userName, String password) throws SQLException {
-        String conUrl = "jdbc:mysql://" + hostName + ":3306/" + dbName + "?verifyServerCertificate=false&useSSL=false&requireSSL=false&serverTimezone=UTC";
+    private static Connection getMySQLConnection(String hostName, String dbName, String userName, String password)
+            throws SQLException {
+        String conUrl = "jdbc:mysql://" + hostName + ":3306/" + dbName +
+                "?verifyServerCertificate=false&useSSL=false&requireSSL=false&serverTimezone=UTC";
         return DriverManager.getConnection(conUrl, userName, password);
     }
 
